20f5a821692037bce8fcb5d6dde8d018744d3ea0
root <xcyang@umd.edu>
2024-05-15 02:22:53 +0000

    set batch_size to 1 in the MultiProcessingSyncWrapper in environment.py, training on NetHackChallenge

--- experiment_code/hackrl/environment.py
+++ experiment_code/hackrl/environment.py
@@ -44,7 +44,7 @@ def create_env(flags, curriculum_components=None, task_queue=None, update_queue=
         penalty_step=flags.penalty_step,
         penalty_time=flags.penalty_time,
         penalty_mode=flags.fn_penalty_step,
-        #no_progress_timeout=150,
+        no_progress_timeout=150,
     )
     if flags.env in ("staircase", "pet", "oracle"):
         kwargs.update(reward_win=flags.reward_win, reward_lose=flags.reward_lose)
@@ -68,7 +68,7 @@ def create_env(flags, curriculum_components=None, task_queue=None, update_queue=
             curriculum_components,
             update_on_step=False,
             task_space=env.task_space,
-            buffer_size=4,
+            buffer_size=1,
         )
     # if task_queue is not None and update_queue is not None:
     #     env = MultiProcessingSyncWrapper(
